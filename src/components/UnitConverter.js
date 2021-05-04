823d50eff0691437b706961bef3bbe60adc1e314
lala-lee-jobs <lala.lee.jobs@gmail.com>
2021-05-05 07:00:31 +0800

    3-6 React元件間的資料傳遞

---
+++ src/components/UnitConverter.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const UnitConverter = (props) => {
+  const { inputValue, handleInputChange } = props;
+  return (
+    <div className="converter">
+    <div className="flex-1">
+      <div className="converter-title">Set</div>
+      <input type="number" className="input-number" min="0"
+      onChange={handleInputChange} value={inputValue}/>
+    </div>
+    <span className="angle-icon fa-2x" style={{marginTop:30}}>
+      <i className="fas fa-angle-right"></i>
+    </span>
+    <div className="text-right flex-1">
+      <div className="converter-title">Show</div>
+      <input type="text" className="input-number text-right" disabled value={inputValue/8}/>
+    </div>
+  </div>
+  );
+};
+
+export default UnitConverter;
